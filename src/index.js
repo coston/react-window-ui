a9f4ac7fe7bc06296e13c01605ca485b5302984d
Coston Perkins <coston.perkins@ua.edu>
2018-05-02 10:25:55 -0500

    Format code

--- src/index.js
+++ src/index.js
@@ -8,12 +8,11 @@ export const Browser = styled.section.attrs({
   background: props => props.background || '#fff',
   border: props => props.border || '0.05rem solid #AEAEAE',
   divider: props => props.divider || '0.05rem solid #ccc',
-
 })`
   position: relative;
   border: ${props => props.border};
   border-radius: 0.5rem;
-  box-shadow: 0 20px 68px rgba(0, 0, 0, .55);
+  box-shadow: 0 20px 68px rgba(0, 0, 0, 0.55);
   background: ${props => props.background};
   max-width: 100%;
   min-height: 22rem;
@@ -24,12 +23,15 @@ export const Browser = styled.section.attrs({
     position: absolute;
     width: 6px;
     height: 6px;
-    top: .8rem;
-    left: .8rem;
+    top: 0.8rem;
+    left: 0.8rem;
     z-index: 2;
     border-radius: 50%;
-    background: ${props => props.grayscale ? '#8D8D92' : '#f85955'};
-    box-shadow: ${props => props.grayscale ? '0 0 0 0.2em #8D8D92,  1.1em 0 0 0.2em #8D8D92, 2.2em 0 0 0.2em #8D8D92' : '0 0 0 0.2em #f85955,  1.2em 0 0 0.2em #fbbe3f, 2.3em 0 0 0.2em #45cc4b'};
+    background: ${props => (props.grayscale ? '#8D8D92' : '#f85955')};
+    box-shadow: ${props =>
+      props.grayscale
+        ? '0 0 0 0.2em #8D8D92,  1.1em 0 0 0.2em #8D8D92, 2.2em 0 0 0.2em #8D8D92'
+        : '0 0 0 0.2em #f85955,  1.2em 0 0 0.2em #fbbe3f, 2.3em 0 0 0.2em #45cc4b'};
   }
   &:before {
     content: '';
@@ -54,13 +56,12 @@ export const Terminal = styled.section.attrs({
   background: props => props.background || '#000',
   border: props => props.border || '0.05rem solid #000',
   divider: props => props.divider || '0.05rem solid #000',
-
 })`
   position: relative;
   border: ${props => props.border};
   border-radius: 0.5rem;
   color: white;
-  box-shadow: 0 20px 68px rgba(0, 0, 0, .55);
+  box-shadow: 0 20px 68px rgba(0, 0, 0, 0.55);
   background: ${props => props.background};
   max-width: 100%;
   min-height: 22rem;
@@ -71,12 +72,15 @@ export const Terminal = styled.section.attrs({
     position: absolute;
     width: 6px;
     height: 6px;
-    top: .8rem;
-    left: .8rem;
+    top: 0.8rem;
+    left: 0.8rem;
     z-index: 2;
     border-radius: 50%;
-    background: ${props => props.grayscale ? '#8D8D92' : '#f85955'};
-    box-shadow: ${props => props.grayscale ? '0 0 0 0.2em #8D8D92,  1.1em 0 0 0.2em #8D8D92, 2.2em 0 0 0.2em #8D8D92' : '0 0 0 0.2em #f85955,  1.1em 0 0 0.2em #fbbe3f, 2.2em 0 0 0.2em #45cc4b'};
+    background: ${props => (props.grayscale ? '#8D8D92' : '#f85955')};
+    box-shadow: ${props =>
+      props.grayscale
+        ? '0 0 0 0.2em #8D8D92,  1.1em 0 0 0.2em #8D8D92, 2.2em 0 0 0.2em #8D8D92'
+        : '0 0 0 0.2em #f85955,  1.1em 0 0 0.2em #fbbe3f, 2.2em 0 0 0.2em #45cc4b'};
   }
   &:before {
     content: '';
@@ -102,13 +106,12 @@ export const MacTerminal = styled.section.attrs({
   background: props => props.background || '#000',
   border: props => props.border || '0.0rem solid #1D1F21',
   divider: props => props.divider || '0.05rem solid #1D1F21',
-
 })`
   position: relative;
   border: ${props => props.border};
   border-radius: 0.5rem;
   color: white;
-  box-shadow: 0 20px 68px rgba(0, 0, 0, .55);
+  box-shadow: 0 20px 68px rgba(0, 0, 0, 0.55);
   background: ${props => props.background};
   max-width: 100%;
   min-height: 22rem;
@@ -119,13 +122,16 @@ export const MacTerminal = styled.section.attrs({
     position: absolute;
     width: 6px;
     height: 6px;
-    top: .50rem;
-    left: .6rem;
+    top: 0.5rem;
+    left: 0.6rem;
     z-index: 2;
     border-radius: 50%;
     background: #f85955;
-    background: ${props => props.grayscale ? '#8D8D92' : '#f85955'};
-    box-shadow: ${props => props.grayscale ? '0 0 0 0.2em #8D8D92,  1.1em 0 0 0.2rem #8D8D92, 2.2em 0 0 0.2rem #8D8D92' : '0 0 0 0.2rem #f85955,  1.1rem 0 0 0.2rem #fbbe3f, 2.2rem 0 0 0.2rem #45cc4b'};
+    background: ${props => (props.grayscale ? '#8D8D92' : '#f85955')};
+    box-shadow: ${props =>
+      props.grayscale
+        ? '0 0 0 0.2em #8D8D92,  1.1em 0 0 0.2rem #8D8D92, 2.2em 0 0 0.2rem #8D8D92'
+        : '0 0 0 0.2rem #f85955,  1.1rem 0 0 0.2rem #fbbe3f, 2.2rem 0 0 0.2rem #45cc4b'};
   }
   &:before {
     content: '';
